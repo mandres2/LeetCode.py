871dcefd2da2e97720bb48435bf9ee1661f14529
mandres2 <limbo.linux.dev@gmail.com>
2023-04-23 17:10:26 -1000

    Create lcp_1046.py

---
+++ lcp_1046.py
@@ -0,0 +1,52 @@
+# 1046. Last Stone Weight
+
+"""
+You are given an array of integers stones where stones[i] is the weight of the ith stone.
+
+We are playing a game with the stones. On each turn, we choose the heaviest two stones and smash them together. Suppose the heaviest two stones have weights x and y with x <= y. The result of this smash is:
+
+If x == y, both stones are destroyed, and
+If x != y, the stone of weight x is destroyed, and the stone of weight y has new weight y - x.
+At the end of the game, there is at most one stone left.
+
+Return the weight of the last remaining stone. If there are no stones left, return 0.
+
+ 
+
+Example 1:
+
+Input: stones = [2,7,4,1,8,1]
+Output: 1
+Explanation: 
+We combine 7 and 8 to get 1 so the array converts to [2,4,1,1,1] then,
+we combine 2 and 4 to get 2 so the array converts to [2,1,1,1] then,
+we combine 2 and 1 to get 1 so the array converts to [1,1,1] then,
+we combine 1 and 1 to get 0 so the array converts to [1] then that's the value of the last stone.
+
+Solution:
+Approach 1: Array-Based Simulation
+Intuition
+Conceptually, the simplest way we could solve this problem is to repeatedly search for the 2 largest stones in the array, delete them, and then if they are not the same size, add the new stone size back in. We can repeat this process until there is only one stone left.
+
+Algorithm
+Because the array is not sorted, there is no need to preserve the original order. Removals should be done by swapping with the last value, not by shuffling all values along.
+Time-Complexity: O(N^2)
+Space Complexity: O(N) or O(1)
+"""
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+
+        def remove_largest():
+            index_of_largest = stones.index(max(stones))
+            # Swap the stone to be removed with the end.
+            stones[index_of_largest], stones[-1] = stones[-1], stones[index_of_largest]
+            return stones.pop()
+
+        while len(stones) > 1:
+            stone_1 = remove_largest()
+            stone_2 = remove_largest()
+            if stone_1 != stone_2:
+                stones.append(stone_1 - stone_2)
+
+        return stones[0] if stones else 0
+
